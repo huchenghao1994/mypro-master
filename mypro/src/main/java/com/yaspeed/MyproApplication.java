d13ce54e99f30f9b6bdc4232e00f6d8db88e5ed8
huchenghao1994 <893541776@qq.com>
2019-03-22 17:23:56 +0800

    1

---
+++ mypro/src/main/java/com/yaspeed/MyproApplication.java
@@ -0,0 +1,12 @@
+package com.yaspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyproApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyproApplication.class, args);
+	}
+}

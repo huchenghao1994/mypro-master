4a92d95dd00ef48412a414f24a279753fb6a4d11
huchenghao1994 <893541776@qq.com>
2019-03-22 17:50:46 +0800

    代码生成器

---
+++ mypro-generator/src/test/java/com/yaspeed/GeneratorApplication.java
@@ -0,0 +1,36 @@
+package com.yaspeed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GeneratorApplication.class)
+public class GeneratorApplication {
+
+    @Test
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<>();
+		boolean overwrite = true;
+		String path=this.getClass().getResource("/").getPath();
+		//指定 逆向工程配置文件
+        File configFile=new File(path+"/mybatis-generator.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	} 
+}
